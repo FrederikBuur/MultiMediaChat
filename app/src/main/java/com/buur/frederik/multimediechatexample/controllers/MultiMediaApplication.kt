843a25e05dbaec83255a0ee7fb651818371fa1d9
Frederik Buur <frederik_buur@hotmail.com>
2018-10-03 16:02:03 +0200

    App: Implemented Leakcanary

--- app/src/main/java/com/buur/frederik/multimediechatexample/controllers/MultiMediaApplication.kt
+++ app/src/main/java/com/buur/frederik/multimediechatexample/controllers/MultiMediaApplication.kt
@@ -1,4 +1,24 @@
 package com.buur.frederik.multimediechatexample.controllers
 
-class MultiMediaApplication {
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class MultiMediaApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupLeakCanary()
+
+    }
+
+    private fun setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
+    }
+
 }
